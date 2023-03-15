190983880570732aba4593945c474e90033a1133
netfireblack <pablo.metalheart@gmail.com>
2023-03-15 17:47:53 -0300

    finalizado a medias

--- sistemaNotas/api.py
+++ sistemaNotas/api.py
@@ -1,5 +1,5 @@
 from .models import Alumno, Asignatura, Nota, Semestre
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .serializers import AlumnoSerializer, AsignaturaSerializer, NotaSerializer, SemestreSerializer
 
 
@@ -7,21 +7,28 @@ class AlumnoViewSet(viewsets.ModelViewSet):
     queryset = Alumno.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = AlumnoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['vcapellido']
     
 
 class AsignaturaViewSet(viewsets.ModelViewSet):
     queryset = Asignatura.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = AsignaturaSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['vcnombre']
 
 
 class NotaViewSet(viewsets.ModelViewSet):
     queryset = Nota.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = NotaSerializer
-    
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['dvalor']
 
 class SemestreViewSet(viewsets.ModelViewSet):
     queryset = Semestre.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = SemestreSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['iano', 'inumero']
--- sistemaNotas/models.py
+++ sistemaNotas/models.py
@@ -1,17 +1,10 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
 class Alumno(models.Model):
-    ialumnoid = models.AutoField(db_column='iAlumnoID', primary_key=True)  # Field name made lowercase.
-    vcnombre = models.CharField(db_column='vcNombre', max_length=100, blank=True, null=True)  # Field name made lowercase.
-    vcapellido = models.CharField(db_column='vcApellido', max_length=100, blank=True, null=True)  # Field name made lowercase.
+    ialumnoid = models.AutoField(db_column='iAlumnoID', primary_key=True)
+    vcnombre = models.CharField(db_column='vcNombre', max_length=100, blank=True, null=True)
+    vcapellido = models.CharField(db_column='vcApellido', max_length=100, blank=True, null=True) 
 
     class Meta:
         managed = False
@@ -19,8 +12,8 @@ class Alumno(models.Model):
 
 
 class Asignatura(models.Model):
-    iasignaturaid = models.AutoField(db_column='iAsignaturaID', primary_key=True)  # Field name made lowercase.
-    vcnombre = models.CharField(db_column='vcNombre', max_length=100, blank=True, null=True)  # Field name made lowercase.
+    iasignaturaid = models.AutoField(db_column='iAsignaturaID', primary_key=True)
+    vcnombre = models.CharField(db_column='vcNombre', max_length=100, blank=True, null=True)
 
     class Meta:
         managed = False
@@ -28,11 +21,11 @@ class Asignatura(models.Model):
 
 
 class Nota(models.Model):
-    inotaid = models.AutoField(db_column='iNotaID', primary_key=True)  # Field name made lowercase.
-    ialumnoid = models.ForeignKey(Alumno, models.DO_NOTHING, db_column='iAlumnoID', blank=True, null=True)  # Field name made lowercase.
-    isemestreid = models.ForeignKey('Semestre', models.DO_NOTHING, db_column='iSemestreID', blank=True, null=True)  # Field name made lowercase.
-    iasignaturaid = models.ForeignKey(Asignatura, models.DO_NOTHING, db_column='iAsignaturaID', blank=True, null=True)  # Field name made lowercase.
-    dvalor = models.DecimalField(db_column='dValor', max_digits=2, decimal_places=1, blank=True, null=True)  # Field name made lowercase.
+    inotaid = models.AutoField(db_column='iNotaID', primary_key=True)
+    ialumnoid = models.ForeignKey(Alumno, models.DO_NOTHING, db_column='iAlumnoID', blank=True, null=True) 
+    isemestreid = models.ForeignKey('Semestre', models.DO_NOTHING, db_column='iSemestreID', blank=True, null=True)
+    iasignaturaid = models.ForeignKey(Asignatura, models.DO_NOTHING, db_column='iAsignaturaID', blank=True, null=True)
+    dvalor = models.DecimalField(db_column='dValor', max_digits=2, decimal_places=1, blank=True, null=True)
 
     class Meta:
         managed = False
@@ -40,9 +33,9 @@ class Nota(models.Model):
 
 
 class Semestre(models.Model):
-    isemestreid = models.AutoField(db_column='iSemestreID', primary_key=True)  # Field name made lowercase.
-    iano = models.IntegerField(db_column='iAno', blank=True, null=True)  # Field name made lowercase.
-    inumero = models.IntegerField(db_column='iNumero', blank=True, null=True)  # Field name made lowercase.
+    isemestreid = models.AutoField(db_column='iSemestreID', primary_key=True)
+    iano = models.IntegerField(db_column='iAno', blank=True, null=True)
+    inumero = models.IntegerField(db_column='iNumero', blank=True, null=True)
 
     class Meta:
         managed = False
